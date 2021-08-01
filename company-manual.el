119e41c46363b79bbac8090a6ee5d9d4185b75ba
savnkk_manjaro <39103342+natask@users.noreply.github.com>
2021-08-01 00:15:14 -0700

    (init): Working implementation

---
+++ company-manual.el
@@ -0,0 +1,80 @@
+;;; company-manual.el --- completely manual company completion completely  -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2021 Natnael Kahssay
+;;
+;; Author: Natnael Kahssay <https://github.com/natask>
+;; Maintainer: Natnael Kahssay <thisnkk@gmail.com>
+;; Created: July 31, 2021
+;; Modified: July 31, 2021
+;; Version: 0.0.1
+;; Homepage: https://github.com/savnkk/company-manual
+;; Package-Requires: ((emacs "24.3") (company "0.6.13"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;; This
+;;
+;;  Description
+;;  Unhooks company from pre-command-hook and post-command-hook, until the user manually uses company functions (company-complete, helm-company (through company-complete), company-complete-manual-begin, company-complete-common).
+;;; Requirements:
+(require 'company)
+;;; Code:
+
+(defun company-manual-start (&optional args)
+  "Prepare company. Pass through ARGS."
+  (add-hook 'pre-command-hook 'company-pre-command nil t)
+  (add-hook 'post-command-hook 'company-post-command nil t)
+  args)
+
+(defun company-manual-end (&optional args)
+  "Disassemble company. Pass through ARGS."
+  (remove-hook 'pre-command-hook 'company-pre-command t)
+  (remove-hook 'post-command-hook 'company-post-command t)
+  args)
+
+(defun company-manual-start-fn ()
+  "Make company-mode functions execute only manually."
+  (remove-hook 'pre-command-hook 'company-pre-command t)
+  (remove-hook 'post-command-hook 'company-post-command t)
+  (remove-hook 'yas-keymap-disable-hook 'company--active-p t)
+  (company-manual-mode-enable))
+
+(defun company-manual-mode-enable ()
+  "Enable company manual mode."
+  (advice-add 'company-manual-begin :after #'company-manual-start)
+  (advice-add 'company-complete-common :after #'company-manual-start)
+  (advice-add 'company-complete-selection :before #'company-manual-start)
+  (advice-add 'company--insert-candidates :after #'company-manual-end)
+  (advice-add 'company-cancel :after #'company-manual-end))
+
+(defun company-manual-mode-disable ()
+  "Disable company manual mode."
+  (advice-remove 'company-manual-begin #'company-manual-start)
+  (advice-remove 'company-complete-common #'company-manual-start)
+  (advice-remove 'company-complete-selection #'company-manual-start)
+  (advice-remove 'company--insert-candidates #'company-manual-end)
+  (advice-remove 'company-cancel #'company-manual-end))
+
+;;;###autoload
+(define-minor-mode company-manual-mode
+  "`company-manual-mode' to operate `company-mode' manually.
+\\{company-manual-mode-map}
+keymap during active completions (`company-manual-active-map'):
+
+\\{company-manual-active-map}"
+  :lighter nil
+  (if company-manual-mode
+      (progn
+        (add-hook 'company-mode-hook
+                  #'company-manual-start-fn nil 't)
+        (company-mode))
+    (remove-hook 'company-mode-hook
+                 #'company-manual-start-fn 't)
+    (company-manual-mode-disable)
+    (company-mode 0)))
+
+(define-globalized-minor-mode global-company-manual-mode company-manual-mode company-mode-on)
+
+(provide 'company-manual)
+;;; company-manual.el ends here
